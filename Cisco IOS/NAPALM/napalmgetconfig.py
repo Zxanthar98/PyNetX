2bebe58405333519512fdf52ae5d9f6bc8c341a8
Zxanthar98 <jonathan.a.knowles@gmail.com>
2024-08-20 21:43:57 -0700

    Added a lot of netmiko scripts to Netmiko dir since my lab switches cannot do netconf, and Netmiko and NAPALM are the only Python libs I can use right now.

--- Cisco IOS/NAPALM/napalmgetconfig.py
+++ Cisco IOS/NAPALM/napalmgetconfig.py
@@ -1,10 +1,11 @@
 #had to use pip install napalm --break-system-packages to install napalm
 import napalm
 from pprint import pprint
+import xmltodict
 
 driver = napalm.get_network_driver('ios')
 
-# Defines a dict of devices
+#Defines a dict of devices
 devices = {
     "LABCAT01": {
         "hostname": "10.10.210.2",
@@ -23,7 +24,7 @@ devices = {
     }, 
 }
 
-# Iterates over each devices in the above devices dict
+#Iterates over each devices in the above devices dict
 for device_name, device_info in devices.items():
     # Initialize the driver with device credentials
     device = driver(
@@ -33,15 +34,15 @@ for device_name, device_info in devices.items():
     )
     
     try:
-        # Open the connection to the device
+        #Open the connection to the device
         device.open()
         
-        # Retrieve the running configuration and print output to terminal
-        pprint(device.get_config(retrieve='running', full=False))
+        #Retrieve the running configuration and print output to terminal
+        (device.get_config(retrieve ='running', full=False))
     
     except Exception as e:
         print(f"An error occurred with device {device_name}: {e}")
     
     finally:
-        # Always close the connection to the device
+        #Always close the connection to the device
         device.close()
