328214faedfef6a3bfb54670d87b891af0557757
Zxanthar98 <jonathan.a.knowles@gmail.com>
2024-09-04 21:50:08 -0700

    Added capability to take dynamic action depending on if a mac address is found take 'x' action.

--- Cisco IOS/Netmiko/gettrunkint.py
+++ Cisco IOS/Netmiko/gettrunkint.py
@@ -4,7 +4,7 @@ import json
 #Our list of IPs to connect to
 ip_addresses = ['10.10.210.2', '10.10.210.3', '10.10.210.4']
 
-mac_address = ['e438.8340.84db']
+mac_list = ('00e0.4c69.1015')
 
 #Creates 'ip' variable by iterating over list 'ip_addresses'. 
 for ip in ip_addresses:
@@ -21,21 +21,20 @@ for ip in ip_addresses:
         c.enable()
         output = c.send_command('show interface switchport', use_textfsm=True)
         hostname = c.send_command('show run | sec hostname', use_textfsm=True)
-        #mac_table = c.send_command('show mac address-table', use_textfsm=True)
+        mac_table = c.send_command('show mac address-table', use_textfsm=True) 
         
-        #Pulls interface data from output for later use in command payload.
-        data = json.loads(output)
-        interface_name = data["interface"]
         
-        payload = (f'interface {t}','description test')
-        print(json.dumps(f"Interface Name: {interface_name}", indent=4))
-          
+        for m in mac_table:
+            #mac_address = m['destination_address']
+            if m['destination_address'] == mac_list:
+                print(f"Steamdeck found on {hostname} on {m['destination_port']}\n")
         
-        #Comment here
-        for t in output:
+        for t in output:     
+            interface_name = t['interface'] #gets the 'interface' name out of output by iterating over each dictionary
+            payload = [f'interface {interface_name}', 'description Standard'] 
             if t['mode'] == 'trunk' or  ['admin_mode'] == 'trunk':
-                print(f"Interface {t['interface']} on {hostname} is a trunk.\n")
-            if not ['mode'] == 'trunk' or  ['admin_mode'] == 'trunk':
+                print(f"Interface {t['interface']} on {hostname} is a trunk.\n") 
+            else:
                 c.send_config_set(payload)
             
         #Saves the running-config to the startup
