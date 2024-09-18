51196d2ea9303df297be68ed562e15637b7e1c06
Zxanthar98 <jonathan.a.knowles@gmail.com>
2024-09-17 19:38:25 -0700

    Made changes to getvlans.py to try and configure the vlan name along with placing the vlan name in the vlan id in the vlan DB.

--- Cisco IOS/Netmiko/getvlans.py
+++ Cisco IOS/Netmiko/getvlans.py
@@ -3,6 +3,7 @@ from netmiko import ConnectHandler
 import getpass
 import datetime
 import yaml
+import json
 
 #Grabs the start time of the script to be subtracted from the endtime variable at the end of the script.
 starttime = datetime.datetime.now()
@@ -20,7 +21,7 @@ def iterate_hosts_by_group(inventory, group_name):
     return hosts
     
 #Defining the main function of the script to connect to our switches and check their vlan integrity.  
-def process_switch(switch_ip, username, password, vlanID_DB):
+def process_switch(switch_ip, username, password, vlanid_DB):
     
     try:
         # Define switch parameters
@@ -43,23 +44,32 @@ def process_switch(switch_ip, username, password, vlanID_DB):
         #Presetting the var 'Pass' to True before the script continues.
         Pass = True
                 
-        #Creates a list of vlans from the data in 'output'
-        configured_vlans = {v['vlan_id'] for v in output}
-        '''Example data:
-                    {'999', '1', '1004', '12', '1005', '1002', '1003'}
-                    {'999', '15', '1', '1004', '1005', '1002', '1003', '65'}
-                    {'40', '999', '15', '1', '27', '1004', '43', '12', '1005', '65', '1002', '1003', '41', '42'}
-                    {'999', '15', '99', '1', '27', '1004', '12', '1005', '1002', '1003', '65', '13'}
-            Each of these lists of vlans are from our four lab switches, you can see plainly that only one of them matches our list 'vlanID_DB'.
+        #Creates a list of vlan id's from the data in 'output'
+        configured_vlans_id = {v['vlan_id'] for v in output}
+        
         '''
+        Example data:
+                    {'40', '999', '15', '1', '27', '1004', '43', '12', '1005', '65', '1002', '1003', '41', '42'}
+                    This is a list of all the vlan id's on one of  our switches in the lab. 
+        '''
+        
+        #Creates a list of vlan names from the data in 'output'
+        configure_vlans_name = {v['vlan_name'] for v in output}
+        '''
+        Example data:
+                    {'default', 'fddi-default', 'LAB', 'fddinet-default', 'trnet-default', 'eLock-VLAN-27', 'WiFi', 'token-ring-default'}
+                    This is a list of all the vlan names on one of  our switches in the lab.        
+        '''
+        
+        #print(configure_vlans_name)
 
         #Iterates over our list of vlan id's and checks that list against the configured vlans on each switch in the above data. 
-        for vlan in vlanID_DB:
-            vlan_config = (f'vlan {vlan}', f'name {var_here}')
-            if vlan not in configured_vlans:
+        for vlan, vlan_name in vlanid_DB, vlanname_DB:
+            vlan_config = (f'vlan {vlan}', f'name {vlan_name}')
+            if vlan not in configured_vlans_id:
                 Pass = False
-                c.send_config_set(vlan_config)
-                print(f'!!{hostname} is missing vlan {vlan} from its vlan DB!!\n')
+                #c.send_config_set(vlan_config)
+                #print(f'{hostname} is missing vlan {vlan} from its vlan DB!!\n')
                 
         #If the variable 'Pass' hasn't been overwritten to False, this means that the switch has all of the vlans in our static DB configured locally.         
         if Pass:
@@ -68,6 +78,10 @@ def process_switch(switch_ip, username, password, vlanID_DB):
 
     except Exception as e:
         print(f"Error processing switch {switch_ip}: {e}")
+        
+        '''
+        Boilerplate code below this point. This code generalizable and the ip_addresses data is populated by loading a 'hosts.yaml' file. 
+        '''
     
 #Load the inventory file hosts.yaml in the local dir. Str value in load_inventory_yaml is the filepath to the inv file.    
 inventory = load_inventory_yaml('hosts.yaml')
@@ -79,14 +93,14 @@ group_name = input("Please enter the group name (e.g., 'LAB', 'PROD'): ")
 ip_addresses = iterate_hosts_by_group(inventory, group_name)
 username = input("Please enter your username: ")
 password = getpass.getpass("Please enter your password: ")
-vlanID_DB = ['1','99']
-vlanname_DB = ['default', 'LAB_VLAN']
+vlanid_DB = ['2','99']
+vlanname_DB = ['DMZ', 'Servers']
 
 #Run the script concurrently for each IP address in our inventory file. 
 with ThreadPoolExecutor(max_workers=len(ip_addresses)) as executor:
     
     #Runs process switch and stores result in var futures to be iterated over later.  
-    futures = {executor.submit(process_switch, ip, username, password, vlanID_DB): ip for ip in ip_addresses}
+    futures = {executor.submit(process_switch, ip, username, password, vlanid_DB): ip for ip in ip_addresses}
 
     #Processes the variable future as they are completed.
     for future in as_completed(futures):
